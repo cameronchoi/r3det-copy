dc794c66667c7603fe3c3dc64bfb542c051c7078
Cameron Choi <cameron.choi99@gmail.com>
2021-10-28 22:00:12 +1100

    try base

--- configs/_base_/schedules/schedule_1x.py
+++ configs/_base_/schedules/schedule_1x.py
@@ -1,11 +1,15 @@
 # optimizer
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
-optimizer_config = dict(grad_clip=None)
+optimizer = dict(type='SGD',
+                 lr=4e-3,
+                 momentum=0.9,
+                 weight_decay=0.0001,
+                 )  # paramwise_options=dict(bias_lr_mult=2, bias_decay_mult=0))
+optimizer_config = dict(grad_clip=dict(max_norm=10, norm_type=2))
 # learning policy
 lr_config = dict(
     policy='step',
     warmup='linear',
-    warmup_iters=500,
-    warmup_ratio=0.001,
-    step=[8, 11])
-total_epochs = 12
+    warmup_iters=5000,
+    warmup_ratio=0.1,
+    step=[12, 16, 20])
+total_epochs = 24
